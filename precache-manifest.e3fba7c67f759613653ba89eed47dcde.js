ee00fd893714953e5d7c81cc7ecfbce767699bf0
Enea <eneasecondary@gmail.com>
2018-10-21 21:00:01 -0700

    Updates

---
+++ precache-manifest.e3fba7c67f759613653ba89eed47dcde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "171ffa67796834b06d2a",
+    "url": "/Memory-Card-Game/static/js/runtime~main.171ffa67.js"
+  },
+  {
+    "revision": "704930108348e1219e78",
+    "url": "/Memory-Card-Game/static/js/main.70493010.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/Memory-Card-Game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "704930108348e1219e78",
+    "url": "/Memory-Card-Game/static/css/main.8718105b.chunk.css"
+  },
+  {
+    "revision": "74615f8a0857d47d5023957e44a311ef",
+    "url": "/Memory-Card-Game/index.html"
+  }
+];
